f69bbdee234e0e03bf1b51bbd926b61e2905cc0a
Melody Ho <hyho.melody@gmail.com>
2023-10-04 16:08:03 -0400

    Add client-side validation and loading state to sign-in form

--- src/app/sign-in/_components/sign-in-form.js
+++ src/app/sign-in/_components/sign-in-form.js
@@ -1,24 +1,74 @@
 "use client";
 
+/// imports ///
 import authCredentials from "../_actions/auth-credentials";
-import { experimental_useFormState as useFormState } from "react-dom";
+import { useState } from "react";
+import {
+  experimental_useFormState as useFormState,
+  experimental_useFormStatus as useFormStatus,
+} from "react-dom";
 
-export default function SignInForm() {
-  // TO DO: client side form validation //
-  const [formState, formAction] = useFormState(authCredentials, "initial");
+/// children components ///
+function FormFields() {
+  // client-side validation //
+  // username
+  const [usernameState, setUsernameState] = useState(null);
+  function handleUsernameChange(e) {
+    setUsernameState(e.target.value);
+  }
+  // password
+  const [passwordState, setPasswordState] = useState(null);
+  function handlePasswordChange(e) {
+    setPasswordState(e.target.value);
+  }
+  // form status initialization //
+  const { pending } = useFormStatus();
+
   return (
-    <form action={formAction}>
+    <>
       <ul>
         <li>
           <label htmlFor="username">username</label>
-          <input id="username" name="username" type="text"></input>
+          <input
+            id="username"
+            name="username"
+            onBlur={handleUsernameChange}
+            onChange={handleUsernameChange}
+            type="text"
+          ></input>
+          <p aria-hidden={usernameState !== ""}>
+            {usernameState === "" ? "Please enter your username." : ""}
+          </p>
         </li>
         <li>
           <label htmlFor="password">password</label>
-          <input id="password" name="password" type="password"></input>
+          <input
+            id="password"
+            name="password"
+            onBlur={handlePasswordChange}
+            onChange={handlePasswordChange}
+            type="password"
+          ></input>
+          <p aria-hidden={passwordState !== ""}>
+            {passwordState === "" ? "Please enter your password." : ""}
+          </p>
         </li>
       </ul>
-      <button>Log in</button>
+      <button disabled={!usernameState || !passwordState || pending}>
+        Log in
+      </button>
+    </>
+  );
+}
+
+/// main component ///
+export default function SignInForm() {
+  // server-side response handling //
+  const [formState, formAction] = useFormState(authCredentials, "initial");
+
+  return (
+    <form action={formAction}>
+      <FormFields />
       <h2>
         {formState === "invalid"
           ? "Invalid credentials."
