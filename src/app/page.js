3951ddf3f4fb45f3428a79f0ff368b7e70a9d9e3
Melody Ho <hyho.melody@gmail.com>
2023-10-05 15:45:58 -0400

    Get user data when accessing homepage with authenticated session

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,34 @@
 /// imports ///
 import { getUserSession } from "./(authentication)/_utils/sessions";
 import { headers } from "next/headers";
+import rds from "@/database/rds";
 import { redirect } from "next/navigation";
 import styles from "./page.module.css";
 
 /// private ///
-async function getUser() {
+async function getUserId() {
   const reqHeaders = headers();
   const session = await getUserSession(reqHeaders);
   if (!session) redirect("/sign-in");
-  return session.user;
+  return session.user.id;
+}
+
+async function getUserData() {
+  const userId = await getUserId();
+  try {
+    const user = await rds.models.User.findByPk(userId, {
+      attributes: { exclude: ["password", "createdAt", "updatedAt"] },
+    });
+    // TO DO: handle if user not found //
+    return user;
+  } catch (error) {
+    // TO DO: error handling //
+  }
 }
 
 /// main component ///
 export default async function Home() {
-  const user = await getUser();
+  const user = await getUserData();
 
-  return <main className={styles.main}>{user.id}</main>;
+  return <main className={styles.main}>{`Hi, ${user.firstName}!`}</main>;
 }
