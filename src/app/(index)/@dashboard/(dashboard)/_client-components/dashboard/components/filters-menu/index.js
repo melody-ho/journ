4253bedf7dbb79dfa3f9291d2aeda31f32cc0b01
Melody Ho <hyho.melody@gmail.com>
2023-11-16 16:28:02 -0500

    Add tag search to filters menu

--- src/app/(index)/@dashboard/(dashboard)/_client-components/dashboard/components/filters-menu/index.js
+++ src/app/(index)/@dashboard/(dashboard)/_client-components/dashboard/components/filters-menu/index.js
@@ -3,18 +3,44 @@
 import { useRef, useState } from "react";
 
 export default function FiltesMenu({ passFilters, userTags }) {
+  // initialize states and refs //
   const [startDate, setStartDate] = useState(null);
   const [endDate, setEndDate] = useState(null);
+  const [selectedTagIds, setSelectedTagIds] = useState([]);
+  const [matchedTags, setMatchedTags] = useState([...userTags]);
   const formRef = useRef(null);
   const startDateInput = useRef(null);
   const endDateInput = useRef(null);
 
+  // define event handlers //
   function updateStartDate() {
     setStartDate(startDateInput.current.value);
   }
 
   function updateEndDate() {
     setEndDate(endDateInput.current.value);
+  }
+
+  function updateSelectedTagIds(e) {
+    if (e.target.checked) {
+      setSelectedTagIds((selectedTagIds) => [
+        ...selectedTagIds,
+        e.target.value,
+      ]);
+    } else {
+      const newSelectedTags = selectedTagIds.filter(
+        (selectedTagId) => selectedTagId !== e.target.value,
+      );
+      setSelectedTagIds(newSelectedTags);
+    }
+  }
+
+  function updateMatchedTags(e) {
+    const regex = new RegExp(e.target.value, "i");
+    const newMatchedTags = userTags.filter((userTag) =>
+      userTag.name.match(regex),
+    );
+    setMatchedTags(newMatchedTags);
   }
 
   function applyFilters(e) {
@@ -68,17 +94,49 @@ export default function FiltesMenu({ passFilters, userTags }) {
       </fieldset>
       <fieldset>
         <h2>Tags</h2>
-        {userTags.map((userTag) => (
-          <div key={userTag.id}>
-            <input
-              id={userTag.id}
-              name="tags"
-              type="checkbox"
-              value={userTag.id}
-            ></input>
-            <label htmlFor={userTag.id}>{userTag.name}</label>
+        <div>
+          <h3>Selected Tags</h3>
+          {userTags.map((userTag) => {
+            if (selectedTagIds.includes(userTag.id)) {
+              return (
+                <div key={userTag.id}>
+                  <input
+                    checked
+                    id={userTag.id}
+                    name="tags"
+                    onChange={updateSelectedTagIds}
+                    type="checkbox"
+                    value={userTag.id}
+                  />
+                  <label htmlFor={userTag.id}>{userTag.name}</label>
+                </div>
+              );
+            }
+          })}
+        </div>
+        <div>
+          <h3>Other Tags</h3>
+          <div>
+            <label htmlFor="tagSearch">Search tags:</label>
+            <input id="tagSearch" onChange={updateMatchedTags} type="search" />
           </div>
-        ))}
+          {matchedTags.map((matchedTag) => {
+            if (!selectedTagIds.includes(matchedTag.id)) {
+              return (
+                <div key={matchedTag.id}>
+                  <input
+                    id={matchedTag.id}
+                    name="tags"
+                    onChange={updateSelectedTagIds}
+                    type="checkbox"
+                    value={matchedTag.id}
+                  />
+                  <label htmlFor={matchedTag.id}>{matchedTag.name}</label>
+                </div>
+              );
+            }
+          })}
+        </div>
       </fieldset>
       <button>Apply Filters</button>
     </form>
