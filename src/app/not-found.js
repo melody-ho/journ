161ab8e0e221450cea2f9c707f7b00e9114f77a9
Melody Ho <hyho.melody@gmail.com>
2024-01-01 20:58:05 -0800

    Implement custom 404 page

---
+++ src/app/not-found.js
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import styles from "./not-found.module.css";
+import ThemedImage from "@/helper-components/themed-image";
+
+export default function NotFound() {
+  return (
+    <div className={styles.component}>
+      <header className={styles.header}>
+        <ThemedImage alt="Journ logotype" imageName="logotype" />
+      </header>
+      <main className={styles.main}>
+        <p className={styles.text}>Oops! This page does not exist.</p>
+        <Link className={styles.link} href="/">
+          Back to home →
+        </Link>
+      </main>
+    </div>
+  );
+}
