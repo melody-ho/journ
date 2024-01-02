b28473bdbaa89cdcdb2ba4c6cc6633f30048ff4d
Melody Ho <hyho.melody@gmail.com>
2024-01-02 14:02:05 -0800

    Refactor error handling for getting user and user tags from database

--- src/app/(pages)/_helper-functions/get-user.js
+++ src/app/(pages)/_helper-functions/get-user.js
@@ -6,11 +6,7 @@ import rds from "@/database/rds";
 /// main ///
 export default async function getUser() {
   const userId = await getUserId(headers());
-  try {
-    return await rds.models.User.findByPk(userId, {
-      attributes: { exclude: ["password", "createdAt", "updatedAt"] },
-    });
-  } catch (error) {
-    // TO DO: error handling //
-  }
+  return await rds.models.User.findByPk(userId, {
+    attributes: { exclude: ["password", "createdAt", "updatedAt"] },
+  });
 }
--- src/app/(pages)/_server-actions/get-user-tags.js
+++ src/app/(pages)/_server-actions/get-user-tags.js
@@ -5,17 +5,13 @@ import rds from "@/database/rds";
 
 /// main ///
 export default async function getUserTags(userId) {
-  try {
-    return await rds.models.Tag.findAll({
-      include: {
-        model: rds.models.User,
-        where: { id: userId },
-        attributes: [],
-      },
-      order: [["name", "ASC"]],
-      raw: true,
-    });
-  } catch (error) {
-    // TO DO: error handling //
-  }
+  return await rds.models.Tag.findAll({
+    include: {
+      model: rds.models.User,
+      where: { id: userId },
+      attributes: [],
+    },
+    order: [["name", "ASC"]],
+    raw: true,
+  });
 }
