7db4f3fda6633d236f7609e7580812a787ddd0a8
Melody Ho <hyho.melody@gmail.com>
2024-01-31 14:16:55 -0500

    Update Sequelize and S3 configurations to include production environment

--- src/database/s3/index.js
+++ src/database/s3/index.js
@@ -1,17 +1,16 @@
 import { S3Client } from "@aws-sdk/client-s3";
 
-const config = {
-  REGION: process.env.S3_REGION,
-  ACCESS_KEY: process.env.S3_ACCESS_KEY,
-  SECRET: process.env.S3_SECRET,
-};
+const config =
+  process.env.NODE_ENV === "production"
+    ? { region: process.env.S3_REGION }
+    : {
+        region: process.env.S3_REGION,
+        credentials: {
+          accessKeyId: process.env.S3_ACCESS_KEY,
+          secretAccessKey: process.env.S3_SECRET,
+        },
+      };
 
-const s3 = new S3Client({
-  region: config.REGION,
-  credentials: {
-    accessKeyId: config.ACCESS_KEY,
-    secretAccessKey: config.SECRET,
-  },
-});
+const s3 = new S3Client(config);
 
 export default s3;
--- src/database/sequelize/config/config.js
+++ src/database/sequelize/config/config.js
@@ -8,4 +8,13 @@ module.exports = {
     dialect: "mysql",
     dialectModule: require("mysql2"),
   },
+  production: {
+    host: process.env.SQL_HOST,
+    port: process.env.SQL_PORT,
+    username: process.env.SQL_USER,
+    password: process.env.SQL_PASSWORD,
+    database: process.env.SQL_DB,
+    dialect: "mysql",
+    dialectModule: require("mysql2"),
+  },
 };
